cd8df7dcf4c6c2fdab7602ae67255c59b9137254
mattBtindall <matthewbtindall@gmail.com>
2023-05-23 14:38:58 +0100

    Get all configurable products

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ const admin = new Magento2Api({
         }),
     }
 })
+
+/**
+ * gets all configurable products
+ * @returns {Array.<Object>}
+ */
+async function getConfigurableProducts() {
+    const { items: configProducts } = await admin.get('products', {
+        params: {
+            fields: "items[sku,custom_attributes,id]",
+            searchCriteria: {
+                currentPage: 1,
+                pageSize: 100,
+                filter_groups: [
+                    {
+                        filters: [
+                            { 'field': 'type_id', 'value': 'configurable', 'condition_type': 'eq'}
+                        ]
+                    }
+                ]
+            }
+        }
+    })
+    return configProducts
+}
